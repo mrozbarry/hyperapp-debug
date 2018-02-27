ca9c31ac07e9fa8bcf62806b8938b62d97bfbd5b
Alex Barry <alex.barry@gmail.com>
2018-02-27 12:56:05 -0500

    Removed build from repo.

--- config/webpack.build.js
+++ config/webpack.build.js
@@ -7,7 +7,7 @@ module.exports = {
 
   output: {
     path: path.resolve(__dirname, '..'),
-    filename: 'index.js',
+    filename: 'bundle.js',
     library: 'withDebug',
     libraryExport: 'debug',
     libraryTarget: 'umd',
