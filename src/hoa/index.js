953a26b6f19c6456ca7d803c3852854758e67390
Alex Barry <alex.barry@gmail.com>
2019-11-23 16:21:16 -0500

    Experimental message batching

--- src/hoa/index.js
+++ src/hoa/index.js
@@ -1,80 +1,9 @@
 import * as subscriptionHelper from './helpers/subscription';
 import * as dispatchHelper from './helpers/dispatch';
 import devtoolBridge from './bridge/devtool';
+import makeCollector from './helpers/messageBatcher';
 
 const randId = () => Math.random().toString(36).slice(2);
-
-const makeCollector = (onComplete) => {
-  let buffer = {};
-  let isFirst;
-
-  const reset = () => {
-    isFirst = false;
-    buffer = {
-      action: null,
-      effects: [],
-      state: null,
-    };
-  };
-
-  const digOutEffects = (effect) => {
-    if (!Array.isArray(effect)) {
-      return [];
-    }
-    if (typeof effect[0] === 'function') {
-      return [[{ effect: effect[0], props: effect[1] }]];
-    }
-    return effect.reduce((effects, effect) => {
-      return [
-        ...effects,
-        ...digOutEffects(effect),
-      ];
-    }, []);
-  };
-
-  const dispatch = (action, props) => {
-    if (Array.isArray(action)) {
-      if (typeof action[0] === 'function') {
-        for (const effect of digOutEffects(action)) {
-          buffer.effects.push(effect);
-        }
-        return;
-      } else {
-        buffer.state = action[0];
-        for (const effect of digOutEffects(action[1])) {
-          buffer.effects.push(effect);
-        }
-        return;
-      }
-    }
-    if (typeof action === 'function') {
-      reset();
-      buffer.action = { action, props };
-      return;
-    }
-    buffer.state = action;
-  };
-
-  const subscriptions = (subscriptionUpdates) => {
-    const action = !buffer.action && isFirst
-      ? { action: { name: 'Init' }, props: null }
-      : buffer.action;
-    onComplete({
-      ...buffer,
-      action,
-      subscriptionUpdates,
-    });
-    reset();
-  };
-
-  reset();
-  isFirst = true;
-
-  return {
-    dispatch,
-    subscriptions,
-  };
-};
 
 export default app => (props) => {
   let dispatch = null;
