377a3cebf44bf7521b7ed098652ba05c018f075b
Alex Barry <alex.barry@gmail.com>
2019-12-07 15:49:56 -0500

    Test the dispatch helper
     - Removed function mapping
     - Maybe 100% coverage?

--- src/hoa/helpers/dispatch.js
+++ src/hoa/helpers/dispatch.js
@@ -1,37 +1,9 @@
-const functionMap = {};
-
-const functionMapAdd = (parent, fn) => {
-  if (!fn.name) {
-    console.warn('Encountered an illegal anonymous method used as an action or effect');
-    return;
-  }
-  const key = `${parent}.${fn.name}`;
-  const old = functionMap[key];
-  if (old && old !== fn) {
-    console.warn(`Encountered two actions or effects named ${fn.name}, but different defintions, cannot continue serialization`);
-    return;
-  }
-  functionMap[key] = fn;
-};
-
-const functionMapGet = (parent, { name }) => {
-  const key = `${parent}.${name}`;
-  return functionMap[key];
-};
-
 const fnToObject = (key, value) => {
   if (typeof value === 'function') {
-    const serialized = { type: 'function', name: value.name };
-    // console.log('serializing a function as', serialized);
-    functionMapAdd(key, value);
-    return serialized;
-  }
-  return value;
-};
-
-const objectToFn = (key, value) => {
-  if (value && value.type === 'function') {
-    return functionMapGet(key, value);
+    return {
+      type: 'function',
+      name: value._name || value.name || '<Anonymous Function>',
+    };
   }
   return value;
 };
@@ -44,10 +16,7 @@ const serializeEventTarget = target => ({
 });
 
 const serializeObject = object => {
-  if (!object) {
-    return object;
-  }
-  if (object instanceof Event) {
+  if (object instanceof window.Event) {
     return {
       target: serializeEventTarget(object.target),
       currentTarget: serializeEventTarget(object.currentTarget),
@@ -56,7 +25,6 @@ const serializeObject = object => {
   try {
     return JSON.parse(JSON.stringify(object, fnToObject));
   } catch (err) {
-    console.log('unable to serialize object', object);
     console.warn(err);
     return object;
   }
@@ -66,10 +34,3 @@ export const serialize = (action, props) => ({
   action: serializeObject(action),
   props: serializeObject(props),
 });
-
-export const deserialize = ({ action, props }) => {
-  return {
-    action: JSON.parse(JSON.stringify(action), objectToFn),
-    props: props ? JSON.parse(JSON.stringify(props), objectToFn) : props,
-  };
-};
